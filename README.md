a056ff19fbb6c30b22f7edc1e3627638ab208b2a
Roman, Greg <greg.roman@teamaol.com>
2015-11-28 17:24:03 -0500

    Added readme, pulling wikipedia content

---
+++ README.md
@@ -0,0 +1,22 @@
+## Getting Started
+```
+//Make sure you have node js installed
+https://nodejs.org/en/download/
+
+//Clone repository from github to your local server
+git clone https://github.com/groman00/wiki-game.git
+
+//Enter working directory
+cd wiki-game
+
+//Install dependencies
+sudo npm install
+
+//Run application
+node app.js
+```
+
+You should be able to access the application at http://localhost:3000
+
+
+
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    request('https://www.wikipedia.org/', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        }
+    });
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('This app is listening at http://%s:%s', host, port);
 });
